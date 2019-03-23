85355296404d5a1f3e4985dbb9bce432d4fee1ab
sharat36 <43617598+sharat36@users.noreply.github.com>
2019-03-23 11:58:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,9 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 var config = {
-    apiKey: "AIzaSyBUq-Ljxz1dATPVN9hsQ04FPQIsbkKE20k",
-    authDomain: "zigger-fab73.firebaseapp.com",
-    databaseURL: "https://zigger-fab73.firebaseio.com",
-    projectId: "zigger-fab73",
-    storageBucket: "zigger-fab73.appspot.com",
-    messagingSenderId: "956151236817"
+    xxx
 };
-
+console.disableYellowBox = true;
 firebase.initializeApp(config);
 
 AppRegistry.registerComponent(appName, () => App);
